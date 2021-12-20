38706e3afca984b708573608994e3a184fa6371a
Sebastien ROY <sebastienroy.fr@gmail.com>
2021-12-20 21:59:24 +0100

    Add i2c scanner

---
+++ code/i2c_scanner/i2c_scanner.ino
@@ -0,0 +1,52 @@
+// I2C address scanner program
+// author : unknown. But thanks to him (her)
+#include <Wire.h>
+
+void setup()
+{
+  Wire.begin();
+  Serial.begin(9600);
+  Serial.println("I2C Scanner");
+}
+
+void loop()
+{
+  byte error, address;
+  int nDevices;
+
+  Serial.println("Scanning...");
+
+  nDevices = 0;
+  for(address = 1; address < 127; address++ )
+  {
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+
+    if (error == 0)
+    {
+      Serial.print("I2C device found at address 0x");
+      if (address < 16)
+        Serial.print("0");
+
+      Serial.print(address,HEX);
+      Serial.println("  !");
+
+      nDevices++;
+    }
+    else if (error==4)
+    {
+      Serial.print("Unknown error at address 0x");
+      if (address < 16)
+        Serial.print("0");
+
+      Serial.println(address,HEX);
+    }
+  }
+
+  if (nDevices == 0)
+    Serial.println("No I2C devices found");
+  else
+    Serial.println("done");
+
+  delay(5000); // wait 5 seconds for next scan
+}
